2fedb0c312968b7701a5d7d7b69f899416d5664c
jiarui <2689258861@qq.com>
2018-11-24 14:16:21 +0800

    hello.py

--- hello.py
+++ hello.py
@@ -3,4 +3,4 @@
 def application(environ, start_response):
     start_response('200 OK' , [('Content-Type','text/html')])
     body='<hl>Hello,%s!</hl>'%(environ['PATH_INFO'][2:]or 'web')#PATH_INFO是从/后的字符算起，1：就是从第一个到最后，2：就是从第二个到最后。
-    return [body.encode('gbk')]
+    return [body.encode('utf-8')]
